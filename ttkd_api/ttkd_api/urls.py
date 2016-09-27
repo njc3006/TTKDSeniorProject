0447bd71112ce8669046a69d9d07a31aa1ddb118
Nick Coriale <njc3006@rit.edu>
2016-09-27 16:18:23 -0400

    Removed index url, and un-needed static import

--- ttkd_api/ttkd_api/urls.py
+++ ttkd_api/ttkd_api/urls.py
@@ -4,9 +4,6 @@ ttkd_api URL Configuration
 from django.conf.urls import url, include
 from rest_framework import routers
 
-# Import the function to serve the UI
-from static_serve import serve_ui
-
 # Import ViewSets
 from .views import UserViewSet, index
 
@@ -16,6 +13,5 @@ router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 
 urlpatterns = [
-	url(r'^$', index)
     url(r'^api/', include(router.urls))
 ]
