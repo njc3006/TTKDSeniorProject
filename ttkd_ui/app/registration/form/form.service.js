771ebd9f9db79796fa5d926ecb09a59fa3e8627e
Curtis Cali <ccali1993@gmail.com>
2016-10-04 13:13:37 -0400

    Add fields for emergency contacts and basic info

--- ttkd_ui/app/registration/form/form.service.js
+++ ttkd_ui/app/registration/form/form.service.js
@@ -1,12 +1,24 @@
 (function() {
 	'use strict';
 
-	angular.module('ttkdApp.registrationFormDirective')
-		.factory('FormService', function() {
-			return {
-				getForm: function(id) {
-					return null;
+	function FormService(basicInfoFields, emergencyContactFields) {
+		return {
+			getForm: function(id) {
+				var fields = [];
+
+				switch (id) {
+					case 'basic_info': fields = basicInfoFields; break;
+					case 'emergency_contacts': fields = emergencyContactFields; break;
+					case 'waiver': fields = []; break;
+					default: fields = null; break;
 				}
-			};
-		});
+
+				return fields;
+			}
+		};
+	}
+
+	FormService.$inject = ['basic_info', 'emergency_contacts'];
+	angular.module('ttkdApp.registrationFormDirective')
+		.factory('FormService', FormService);
 })();
