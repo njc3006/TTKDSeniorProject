282b12b210e105e1eb7d7eee518920ae4c903202
Andrew Deck <ajd4223@rit.edu>
2016-10-04 17:27:27 -0400

    Add Get Student List

--- ttkd_api/ttkd_api/urls.py
+++ ttkd_api/ttkd_api/urls.py
@@ -7,22 +7,28 @@ from rest_framework import routers
 # Import ViewSets
 from .views.attendance_record_views import AttendanceRecordViewSet, AttendanceRecordCreateSet
 from .views.person_views import PersonViewSet
-from .views.program_views import ProgramViewSet, ProgramCreateSet
+from .views.program_views import ProgramViewSet, ProgramCreateSet, StudentList
 from .views.registration_views import RegistrationViewSet, RegistrationCreateSet
 from .views import UserViewSet
 
 router = routers.DefaultRouter()
 
 # Register Viewsets
-router.register(r'^user(s|/\d+)$', UserViewSet)
-router.register(r'^person(s|/\d+)$', PersonViewSet)
-router.register(r'^program(s|/\d+)$', ProgramViewSet)
-router.register(r'^program/create$', ProgramCreateSet, 'program-create')
-router.register(r'^check-in(s|/\d+)$', AttendanceRecordViewSet)
-router.register(r'^check-in/create$', AttendanceRecordCreateSet, 'attendance-create')
-router.register(r'^registration(s|/\d+)$', RegistrationViewSet)
-router.register(r'^registration/create$', RegistrationCreateSet, 'registration-create')
+router.register(r'users', UserViewSet)
+router.register(r'persons', PersonViewSet)
+router.register(r'person/\d+', PersonViewSet)
+router.register(r'programs', ProgramViewSet)
+router.register(r'program/create', ProgramCreateSet, 'program-create')
+router.register(r'check-ins', AttendanceRecordViewSet)
+router.register(r'check-in/\d+', AttendanceRecordViewSet)
+router.register(r'check-in/create', AttendanceRecordCreateSet, 'attendance-create')
+router.register(r'registrations', RegistrationViewSet)
+router.register(r'registration/\d+', RegistrationViewSet)
+router.register(r'registration/create', RegistrationCreateSet, 'registration-create')
+
+myurls = router.urls
+myurls.append(url(r'program/(?P<program_id>.+)/students$', StudentList.as_view()))
 
 urlpatterns = [
-    url(r'^api/', include(router.urls))
+    url(r'^api/', include(myurls)),
 ]
--- ttkd_api/ttkd_api/views/program_views.py
+++ ttkd_api/ttkd_api/views/program_views.py
@@ -1,7 +1,9 @@
 """ProgramViewSet"""
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, generics
 from ..serializers.program_serializer import ProgramSerializer
+from ..serializers.registration_serializer import RegistrationSerializer
 from ..models.program import Program
+from ..models.registration import Registration
 
 
 class ProgramViewSet(viewsets.ReadOnlyModelViewSet):
@@ -14,3 +16,14 @@ class ProgramCreateSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     """Create A Program"""
     queryset = Program.objects.all()
     serializer_class = ProgramSerializer
+
+class StudentList(generics.ListAPIView):
+    serializer_class = RegistrationSerializer
+
+    def get_queryset(self):
+        """
+        This view should return a list of all the purchases for
+        the user as determined by the username portion of the URL.
+        """
+        program_id = self.kwargs['program_id']
+        return Registration.objects.filter(program=Program.objects.get(id=program_id))
