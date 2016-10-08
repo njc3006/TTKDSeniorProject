9e31bb636e964e4f91af3eb1d373f0f3d5a503cd
Nick Coriale <njc3006@rit.edu>
2016-10-07 22:59:23 -0400

    Updated URLS after refactoring with filters

--- ttkd_api/ttkd_api/serializers/registration_serializer.py
+++ ttkd_api/ttkd_api/serializers/registration_serializer.py
@@ -18,6 +18,9 @@ class RegistrationSerializer(serializers.ModelSerializer):
         # No Fields declaration to use all the fields of the model
 
     def create(self, validated_data):
+        """
+        Create a person, their emails, and then register them for a program
+        """
         person_data = validated_data.pop('person')
         email_data = person_data.pop('emails')
 
@@ -28,3 +31,15 @@ class RegistrationSerializer(serializers.ModelSerializer):
 
         registration = Registration.objects.create(person=person, program=validated_data['program'])
         return registration
+
+    def update(self, instance, validated_data):
+        """
+        Update the registration instance. Tho all of the person information has to be passed,
+        only the program is updated via this serializer. This is only ever called for an instance
+        PUT, for example doing a PUT to /api/registrations/9/
+
+        If you want to update a person instance, PUT to the persons endpoint
+        """
+        instance.program = validated_data.get('program', instance.program)
+        instance.save()
+        return instance
