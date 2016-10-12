355b8bf5d5ba7a4d997af9b42bb032c07ab2a492
Curtis Cali <ccali1993@gmail.com>
2016-10-12 18:42:42 -0400

    Integrate Registration form with API

--- ttkd_ui/app/registration/registration.service.js
+++ ttkd_ui/app/registration/registration.service.js
@@ -2,7 +2,7 @@
 	function RegistrationService($http, apiHost) {
 		return {
 			registerStudent: function(formData) {
-				return $http.post(apiHost + '/registration/create', formData);
+				return $http.post(apiHost + '/api/registrations/', formData);
 			}
 		};
 	}
