90c7d249b9d87780e34be558f2e86dca9572db40
Curtis Cali <ccali1993@gmail.com>
2016-10-19 22:05:38 -0400

    finish emergency contact panels

--- ttkd_ui/app/components/students/detail/student.detail.controller.js
+++ ttkd_ui/app/components/students/detail/student.detail.controller.js
@@ -1,14 +1,23 @@
 (function() {
 	function StudentDetailController($scope, $stateParams, StudentsService) {
+		$scope.formatPhoneNumber = function(phone) {
+			if (phone === undefined) {
+				return null;
+			}
+
+			return '(' + phone.substring(0, 3) + ') ' + phone.substring(3, 6) + '-' + phone.substring(6);
+		};
+
 		$scope.studentInfo = {};
 		$scope.primaryEmergencyContact = {};
 		$scope.secondaryEmergencyContact = {};
 		$scope.studentRequestFailed = false;
 
 		StudentsService.getStudent($stateParams.studentId).then(function(response) {
-			$scope.studentInfo = response.data;
-			$scope.primaryEmergencyContact = response.data.emergency_contacts[0];
-			$scope.secondaryEmergencyContact = response.data.emergency_contacts[1];
+			$scope.studentInfo = response.data.person;
+			console.log($scope.studentInfo);
+			$scope.primaryEmergencyContact   = $scope.studentInfo.emergency_contacts[0];
+			$scope.secondaryEmergencyContact = $scope.studentInfo.emergency_contacts[1];
 		}, function(error) {
 			console.error(error);
 			$scope.studentRequestFailed = true;
