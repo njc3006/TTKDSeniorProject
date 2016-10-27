d6dfef18c6f9a039e6abecb3ebd80d3078bba233
Michael Washburn <mdw7326@rit.edu>
2016-10-27 14:36:58 -0400

    Toggle calendar when text is clicked
    and fixed active day font color

--- ttkd_ui/app/main.controller.js
+++ ttkd_ui/app/main.controller.js
@@ -3,8 +3,9 @@
   angular.module('ttkdApp.mainCtrl', [])
     .controller('MainCtrl', ['$scope', function($scope) {
       $scope.showNav = true;
-      $scope.title = 'Home';
+      $scope.title = 'Tioga Tae Kwon Do';
       $scope.currentClass = 'Tiny Ninjas';
+      $scope.loggedIn = false;
     }]);
 
 })();
