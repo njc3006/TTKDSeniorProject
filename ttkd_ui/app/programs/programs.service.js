8dcb79dab526a5cd66b52a8e5b66a9eb7d3b2a5c
Michael Washburn <mdw7326@rit.edu>
2016-10-30 18:04:21 -0400

    Only show active classes on change class modal

--- ttkd_ui/app/programs/programs.service.js
+++ ttkd_ui/app/programs/programs.service.js
@@ -2,14 +2,30 @@
 	angular.module('ttkdApp.programsSvc', ['ttkdApp.constants'])
 		.factory('ProgramsSvc', ['$http', '$filter', 'apiHost', function($http, $filter, apiHost) {
 		return {
+
+			/* Gets every program object */
 			getPrograms: function() {
 				return $http.get(apiHost + '/api/programs');
 			},
 
+			/* gets only programs with active set to true */
 			getActivePrograms: function() {
-				return $http.get(apiHost + '/api/programs');
+				// construct the API call and save it's promise it returns
+				var promise = $http.get(apiHost + '/api/programs');
+				// modify the result to only have active classes included by using a filter
+				promise.then(function onSuccess(response) {
+					var filter = $filter('filter');
+	         		response.data = filter(response.data, {'active': true});
+	         		return response;				
+				}, function onFailure(response) {
+					response.data = [];
+					return response;
+				});
+				// return the original promise with our filtered data
+				return promise;
 			},
 
+			/* get a single program by it's id */
 			getProgram: function(id) {
 				return $http.get(apiHost + '/api/programs/'+id);
 			}
