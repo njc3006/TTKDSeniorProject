eec461aae6a4508a7f8deb04b9a1214a2e231779
Nick Coriale <njc3006@rit.edu>
2016-11-09 22:15:53 -0500

    Fixed documentation of copy paste issue

--- ttkd_api/ttkd_api/views/registration_views.py
+++ ttkd_api/ttkd_api/views/registration_views.py
@@ -23,14 +23,10 @@ class RegistrationViewSet(viewsets.ModelViewSet):
 
 class SimpleRegistrationViewSet(viewsets.ModelViewSet):
     """
-    GET: Returns all Registration Objects To The Route, Or An Instance If Given A PK Filters:
-    program, person
-    POST: Create A Registration Which Includes Creating A New Person and Registering Them To A
-    Program
-    Note: Emails must be of the form [{"email": "email@email.com"}, {"email": "email2@email.com"}]
-    Note: If you want emergency_contact_1 or emergency_contact_2 to be null in the db, DO NOT
-    include them in a JSON object, if you include them but have them set to null this view will
-    break
+    GET: Returns all Registration Objects To The Route with person as a PK,
+    Or An Instance If Given A PK
+    Filters: program, person
+    POST: Create A Registration with an existing person by passing a PK of a person and program
     """
     queryset = Registration.objects.all()
     serializer_class = SimpleRegistrationSerializer
