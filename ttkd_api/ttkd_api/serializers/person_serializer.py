3a0f5990fa5ad613e81e25cbfa37f40c532d09a7
Nick Coriale <njc3006@rit.edu>
2016-11-10 18:09:52 -0500

    Added new fields to belt and stripe models. Also refactored person to have a list of stripes and fixed some other endpoints to have more detail to prevent aditional api calls

--- ttkd_api/ttkd_api/serializers/person_serializer.py
+++ ttkd_api/ttkd_api/serializers/person_serializer.py
@@ -1,5 +1,7 @@
 """PersonSerializer"""
 from rest_framework import serializers
+
+from ..serializers.person_stripe_serializer import PersonStripeSerializer
 from ..serializers.email_serializer import EmailSerializer
 from ..serializers.emergency_contact_serializer import EmergencyContactSerializer
 from ..serializers.person_belt_serializer import PersonBeltSerializer
@@ -12,12 +14,13 @@ class PersonSerializer(serializers.ModelSerializer):
     """
     emails = EmailSerializer(many=True)
     belts = PersonBeltSerializer(many=True, read_only=True)
+    stripes = PersonStripeSerializer(many=True, read_only=True)
     emergency_contact_1 = EmergencyContactSerializer(required=False)
     emergency_contact_2 = EmergencyContactSerializer(required=False)
 
     class Meta:
         model = Person
         fields = ('id', 'first_name', 'last_name', 'dob', 'primary_phone', 'secondary_phone',
-                  'street', 'city', 'zipcode', 'state', 'belts', 'emails',
+                  'street', 'city', 'zipcode', 'state', 'belts', 'stripes', 'emails',
                   'emergency_contact_1', 'emergency_contact_2', 'misc_notes', 'picture_path',
                   'active')
--- ttkd_api/ttkd_api/serializers/person_stripe_serializer.py
+++ ttkd_api/ttkd_api/serializers/person_stripe_serializer.py
@@ -1,5 +1,7 @@
 """PersonStripeSerializer"""
 from rest_framework import serializers
+
+from ..serializers.stripe_serializer import StripeSerializer
 from ..models.person_stripe import PersonStripe
 
 
@@ -7,6 +9,8 @@ class PersonStripeSerializer(serializers.ModelSerializer):
     """
     PersonStripeSerializer Outputs PersonStripe Model as JSON
     """
+    stripe = StripeSerializer()
+
     class Meta:
         model = PersonStripe
         # No Fields declaration to use all the fields of the model
