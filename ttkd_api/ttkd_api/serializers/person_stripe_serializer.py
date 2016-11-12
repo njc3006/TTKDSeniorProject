06a1c987aba49274096c208342d9d06b420cba91
Nick Coriale <njc3006@rit.edu>
2016-11-12 18:24:14 -0500

    Removing the serializers from the person belt and stripe serializers becuase they would pervent the creation of now person belt and stripe records with existing betls and stripes

--- ttkd_api/ttkd_api/serializers/person_stripe_serializer.py
+++ ttkd_api/ttkd_api/serializers/person_stripe_serializer.py
@@ -1,7 +1,6 @@
 """PersonStripeSerializer"""
 from rest_framework import serializers
 
-from ..serializers.stripe_serializer import StripeSerializer
 from ..models.person_stripe import PersonStripe
 
 
@@ -9,7 +8,6 @@ class PersonStripeSerializer(serializers.ModelSerializer):
     """
     PersonStripeSerializer Outputs PersonStripe Model as JSON
     """
-    stripe = StripeSerializer()
 
     class Meta:
         model = PersonStripe
