15cff4bae1566f013142cc5f76cb186e0b9dec14
Curtis Cali <ccali1993@gmail.com>
2016-11-13 13:22:45 -0500

    All Necessary data is now loaded

--- ttkd_ui/app/components/students/detail/attendance/attendance.control.js
+++ ttkd_ui/app/components/students/detail/attendance/attendance.control.js
@@ -1,5 +1,90 @@
 (function() {
-	function StudentAttendanceController($scope, $http, apiHost, SharedDataService) {
+	function StudentAttendanceController($scope, $http, $q, apiHost, SharedDataService) {
+		var uniquePrograms = {};
+
+		$scope.loadCheckIns = function() {
+			var studentId = SharedDataService.getStudentId();
+
+			$http.get(apiHost + '/api/check-ins/?person=' + studentId).then(
+				function(checkinsResponse) {
+					var discoveredProgramIds = {};
+
+					checkinsResponse.data.forEach(function(checkIn) {
+						if (uniquePrograms[checkIn.program] === undefined) {
+							discoveredProgramIds[checkIn.program] = true;
+						}
+					});
+
+					var programPromises = [];
+
+					angular.forEach(discoveredProgramIds, function(value, key) {
+						programPromises.push($http.get(apiHost + '/api/programs/' + key + '/'));
+					});
+
+					$q.all(programPromises).then(
+						function(programResponses) {
+							programResponses.forEach(function(resp) {
+								uniquePrograms[resp.data.id] = resp.data;
+							});
+
+							$scope.checkIns = checkinsResponse.data.map(function(checkIn) {
+								return {
+									date: moment(checkIn.date, 'YYYY-MM-DD').format('MM/DD/YYYY'),
+									program: uniquePrograms[checkIn.program].name
+								};
+							});
+						},
+						function(errors) {
+							// TODO: Error Handling
+						}
+					);
+				},
+				function(error) {
+					// TODO: Error Handling
+				}
+			);
+		};
+
+		$scope.loadEnrolledPrograms = function() {
+			var studentId = SharedDataService.getStudentId();
+
+			$http.get(apiHost + '/api/registrations/?person=' + studentId).then(
+				function(registrations) {
+					var discoveredProgramIds = {};
+
+					registrations.data.forEach(function(registration) {
+						if (uniquePrograms[registration.program] === undefined) {
+							discoveredProgramIds[registration.program] = true;
+						}
+					});
+
+					var programPromises = [];
+
+					angular.forEach(discoveredProgramIds, function(value, key) {
+						programPromises.push($http.get(apiHost + '/api/programs/' + key + '/'));
+					});
+
+					$q.all(programPromises).then(
+						function(programResponses) {
+							programResponses.forEach(function(resp) {
+								uniquePrograms[resp.data.id] = resp.data;
+							});
+
+							$scope.enrolledPrograms = registrations.data.map(function(registration) {
+								return uniquePrograms[registration.program];
+							});
+						},
+						function(errors) {
+							// TODO: Error Handling
+						}
+					);
+				},
+				function(error) {
+					// TODO: Error Handling
+				}
+			);
+		};
+
 		$scope.openCalendar = function(date) {
 			date.open = true;
 		};
@@ -19,31 +104,20 @@
 
 		$scope.checkIns = [];
 
-		var studentId = SharedDataService.getStudentId();
+		$scope.enrolledPrograms = [];
+		$scope.selectedProgram = {};
 
-		$http.get(apiHost + '/api/check-ins/?person=' + studentId).then(
-			function(response) {
-				var uniqueProgramIds = {};
-
-				response.data.forEach(function(checkIn) {
-					if (uniqueProgramIds[checkIn.program] === undefined) {
-						uniqueProgramIds[checkIn.program] = true;
-					}
-				});
-
-				// TODO: For each unique program get a 
-
-				$scope.checkIns = response.data.map(function(checkIn) {
-					checkIn.date = moment(checkIn.date, 'YYYY-MM-DD').format('MM/DD/YYYY');
-					return checkIn;
-				});
-			},
-			function(error) {
-
-			}
-		)
+		$scope.loadEnrolledPrograms();
+		$scope.loadCheckIns();
 	}
 
 	angular.module('ttkdApp.studentDetailCtrl')
-		.controller('StudentAttendanceCtrl', ['$scope', '$http', 'apiHost', 'SharedDataSvc', StudentAttendanceController]);
+		.controller('StudentAttendanceCtrl', [
+			'$scope',
+			'$http',
+			'$q',
+			'apiHost',
+			'SharedDataSvc',
+			StudentAttendanceController
+		]);
 })();
