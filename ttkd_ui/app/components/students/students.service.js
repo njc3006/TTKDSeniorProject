ca2456755766eec49f7fa53cea23653eeedd0e76
Michael Washburn <mdw7326@rit.edu>
2016-11-16 15:32:08 -0500

    Finish merging updates from develop

--- ttkd_ui/app/components/students/students.service.js
+++ ttkd_ui/app/components/students/students.service.js
@@ -3,18 +3,14 @@
 		.factory('StudentsSvc', ['$http', '$q', 'apiHost', function($http, $q, apiHost) {
 			return {
 				getStudent: function(id) {
-					return $q(function(resolve, reject) {
-						$http.get(apiHost + '/api/persons/' + id + '/').then(function(response) {
-							resolve(response.data);
-						}, reject);
-					});
+					return $http.get(apiHost + '/api/persons/' + id + '/');
 				},
 
 				/* post a picture to the api for a student with the given id */
 				changePicture: function(id, picture) {
 					var data = {
 						upload: picture
-					}
+					};
 					return $http.post(apiHost + '/api/person/' + id + '/picture', data);
 				}
 			};
