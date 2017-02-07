a260973d909f66fb4ded6e16b05318206a3bf443
Curtis Cali <ccali1993@gmail.com>
2017-02-07 12:56:39 -0500

    Forgot to commit api code

--- ttkd_api/ttkd_api/views/attendance_record_views.py
+++ ttkd_api/ttkd_api/views/attendance_record_views.py
@@ -2,12 +2,15 @@
 import django_filters
 from django_filters import rest_framework as drf_filters
 from rest_framework import viewsets, filters
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.status import *
+from rest_framework.pagination import PageNumberPagination
 from ..serializers.attendance_record_serializer import AttendanceRecordSerializer, \
     AttendanceRecordSerializerUsingPerson, DetailedAttendanceRecordSerializer
 from ..models.attendance_record import AttendanceRecord
+
+class AttendanceRecordPagination(PageNumberPagination):
+    page_size = 125
+    page_size_query_param = 'page_size'
+    max_page_size = 500
 
 class DateRangeFilter(drf_filters.FilterSet):
     class Meta:
@@ -28,17 +31,19 @@ class AttendanceRecordViewSet(viewsets.ModelViewSet):
     queryset = AttendanceRecord.objects.all()
     serializer_class = AttendanceRecordSerializer
     filter_backends = (filters.DjangoFilterBackend,)
-    filter_class = DateRangeFilter
-    #filter_fields = ('person', 'program', 'date',)
+    filter_fields = ('person', 'program', 'date',)
 
 class DetailedAttendanceRecordViewSet(viewsets.ReadOnlyModelViewSet):
     """
+    GET: Returns All AttendanceRecord Objects To The Route AKA Check-ins with person and program as objects.
+    This is endpoint is paginated, with  between 125 and 500 elements per page
+    Filters: person, program, date__gte, date__lte
     """
     queryset = AttendanceRecord.objects.all()
     serializer_class = DetailedAttendanceRecordSerializer
     filter_backends = (filters.DjangoFilterBackend,)
     filter_class = DateRangeFilter
-    #filter_fields = ('person', 'program',)
+    pagination_class = AttendanceRecordPagination
 
 class AttendanceRecordUsingPersonViewSet(viewsets.ReadOnlyModelViewSet):
     """
