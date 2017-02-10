397b2885c143b168e8ac91f50be00f4046ebe0ec
Andrew Vogler <axv3658@rit.edu>
2017-02-10 14:50:08 -0500

    Added lists of existing belts and stripes to pick from

--- ttkd_ui/app/components/belts-stripes/belts-stripes.controller.js
+++ ttkd_ui/app/components/belts-stripes/belts-stripes.controller.js
@@ -3,17 +3,39 @@
   angular.module('ttkdApp.beltsStripesCtrl', ['ttkdApp.constants'])
 
     .controller('BeltsStripesCtrl', ['$scope', '$rootScope', '$stateParams', 'BeltsStripesService', 
-        function($scope, $rootScope, $filter, $stateParams, StudentListService, ProgramsSvc) {
+        function($scope, $rootScope, $stateParams, StudentListService, ProgramsSvc) {
         $rootScope.showCurrentProgram = !$stateParams.hideCurrentProgram;
 
-        $scope.status = 'new';
-        $scope.type = 'belt';
-        $scope.active = false;
-        $scope.pageTitle = '';
+        $scope.belts = [
+            {
+                name: 'red'
+            },
+            {
+                name: 'blue'
+            },
+            {
+                name: 'green'
+            }
+        ];
 
-        $scope.initPageTitle = function(){
-        	$scope.pageTitle = $scope.status + ' ' + $scope.type;
-        };
+         $scope.stripes = [
+            {
+                name: 'yellow'
+            },
+            {
+                name: 'black'
+            },
+            {
+                name: 'purple'
+            }
+        ];
+
+        $scope.showEdit = false;
+        $scope.active = false;
+        $scope.status = '';
+        $scope.type = '';
+        $scope.pageTitle = '';
+        $scope.currentObj = {};
 
         $scope.save = function(){
         	if($scope.status === 'new'){
@@ -29,11 +51,35 @@
         			//put stripe
         		}
         	}
-
-        	console.log("saved");
         };
 
-       	$scope.initPageTitle();
+        $scope.addNew = function(type){
+            $scope.type = type;
+            $scope.status = 'new';
+            $scope.currentObj = {};
+
+            $scope.showEdit = true;
+
+        }
+
+        $scope.edit = function(type, obj){
+            $scope.type = type;
+            $scope.status = 'edit';
+
+            $scope.showEdit = true;
+            $scope.currentObj = obj;
+
+            $scope.name = $scope.currentObj.name;
+        }
+
+        $scope.backNavigate = function() {
+            $scope.showEdit = false;
+            $scope.active = false;
+            $scope.status = '';
+            $scope.type = '';
+            $scope.pageTitle = '';
+            $scope.currentObj = {};
+        };
 
     }]);
 
