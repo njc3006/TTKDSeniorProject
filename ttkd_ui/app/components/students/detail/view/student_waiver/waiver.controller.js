539e85bba9670f35e104a64c41a30f930fc55669
Nick Coriale <njc3006@rit.edu>
2017-02-22 23:20:22 -0500

    Finished implementing waiver tab. This involved an api change to support a front end library who's url paramters can not be changed, and some small re-works to the waiver controller and html files

--- ttkd_ui/app/components/students/detail/view/student_waiver/waiver.controller.js
+++ ttkd_ui/app/components/students/detail/view/student_waiver/waiver.controller.js
@@ -3,15 +3,15 @@
 		$scope.waivers = [];
 		$scope.hasWaivers = false;
 
-		$scope.uploader = new FileUploader({
-			url: apiHost + '/api/person/' + $stateParams.studentId + '/picture',
-			autoUpload: true,
-		});
+		$scope.uploadWaiver = function(waiverId){
+			document.getElementById('waiver-upload' + waiverId).click();
+        };
 
 		var waiversEndpoint = '';
 
 		$scope.getWaivers = function(){
 			waiversEndpoint = apiHost + '/api/waivers/?person=' + $stateParams.studentId;
+			$scope.waivers = [];
 
 			$http.get(waiversEndpoint).then(
 				function(response){
@@ -19,24 +19,38 @@
 					$scope.hasWaivers = ($scope.waivers.length > 0);
 
 					for(var i = 0; i < $scope.waivers.length; i++){
-						$scope.waivers[i].waiver_url = 'C:/Users/Andrew/Desktop/waivers/waiver_' + (i+ 1) + '.png';						
+
+						var waiver_url = $scope.waivers[i].waiver_url;
+
+						// If the waiver_url is not null lets fix the url by adding the api host
+						if (waiver_url != undefined){
+							$scope.waivers[i].waiver_url = apiHost + '/' + $scope.waivers[i].waiver_url;
+						}
 						$scope.waivers[i].formattedDate = moment($scope.waivers[i]['signature_timestamp']).format('MM/DD/YYYY');
+
+						$scope.waivers[i].waiverUploader = new FileUploader({
+							url : apiHost + '/api/waiver/' + $scope.waivers[i].id + '/image',
+							autoUpload: true
+						});
+						$scope.waivers[i].waiverUploader.onSuccessItem = function () {
+							$scope.getWaivers();
+                        };
 					}
 
 				});
 		};
 
-		$scope.addWaiver = function(waiver){
-			waiversEndpoint = apiHost + '/api/waivers/';
-
-			$http.post(waiversEndpoint, waiver).then(
-				function(response){
-					console.log("successfully added waiver");
-
-				}, function(error){
-					console.log("failed to post");
-				});
-		};
+		// $scope.addWaiver = function(waiver){
+		// 	waiversEndpoint = apiHost + '/api/waivers/';
+        //
+		// 	$http.post(waiversEndpoint, waiver).then(
+		// 		function(response){
+		// 			console.log("successfully added waiver");
+        //
+		// 		}, function(error){
+		// 			console.log("failed to post");
+		// 		});
+		// };
 
 		$scope.getWaivers();
 	}
