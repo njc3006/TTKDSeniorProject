7d1f34fbb1bb2bddb16b9161a5949281c59badeb
Curtis Cali <ccali1993@gmail.com>
2017-02-28 12:01:32 -0500

    fix merge errors

--- ttkd_ui/app/api.interceptor.js
+++ ttkd_ui/app/api.interceptor.js
@@ -1,27 +1,32 @@
 (function() {
   angular.module('ttkdApp.AuthInterceptor', ['ngCookies'])
-    .factory('AuthInterceptor',['$cookies', '$location', '$q', function ($cookies, $location, $q) {
-      return {
-        request: function (config) {
-          if($cookies.getObject('Authorization')){
-            config.headers['Authorization'] = 'Token ' + $cookies.getObject('Authorization').token;
-          }
-          return config;
-        },
-        responseError: function (res) {
-          if(res.status === 401) {
-            $location.path('/');
-            if($cookies.getObject('Authorization') || $rootScope.loggedin) {
-              $cookies.remove('Authorization');
-              $rootScope.currentUser = '';
-              $rootScope.loggedin = false;
-              $rootScope.currentUser = 'Anonymous';
-              $rootScope.userlevel = -1;
-            }
-          }
+    .factory('AuthInterceptor', [
+			'$cookies',
+			'$location',
+			'$rootScope',
+			'$q',
+			 function ($cookies, $location, $rootScope, $q) {
+	      return {
+	        request: function (config) {
+	          if($cookies.getObject('Authorization')){
+	            config.headers['Authorization'] = 'Token ' + $cookies.getObject('Authorization').token;
+	          }
+	          return config;
+	        },
+	        responseError: function (res) {
+	          if(res.status === 401) {
+	            $location.path('/');
+	            if($cookies.getObject('Authorization') || $rootScope.loggedin) {
+	              $cookies.remove('Authorization');
+	              $rootScope.currentUser = '';
+	              $rootScope.loggedin = false;
+	              $rootScope.currentUser = 'Anonymous';
+	              $rootScope.userlevel = -1;
+	            }
+	          }
 
-          return $q.reject(res);
-        },
-      };
-    }]);
+	          return $q.reject(res);
+	        },
+	      };
+    	}]);
 })();
