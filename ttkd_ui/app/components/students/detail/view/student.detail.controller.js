aa3211794e41e6936297a8152898c629591e3e39
Michael Washburn <mdw7326@rit.edu>
2017-03-05 16:53:17 -0500

    Refactor SharedDataSvc - use activeStudent promise

--- ttkd_ui/app/components/students/detail/view/student.detail.controller.js
+++ ttkd_ui/app/components/students/detail/view/student.detail.controller.js
@@ -1,147 +1,125 @@
 (function() {
-	function reformatObject(object) {
-		var reformatted = {};
 
-		for (var field in object) {
-			if (object.hasOwnProperty(field)) {
-				var camelCased = field.replace(/[\-_\s]+(.)?/g, function(match, chr) {
-		      return chr ? chr.toUpperCase() : '';
-		    });
+  function StudentDetailController($scope, $stateParams, StudentsService, apiHost, FileUploader, SharedDataSvc, $cookies) {
+    $scope.apiHost = apiHost;
 
-				camelCased = camelCased.substr(0, 1).toLowerCase() + camelCased.substr(1);
+    /* function to update this student object */
+    var updateStudent = function() {
+      SharedDataSvc.getStudent($stateParams.studentId).then(
+        function(student) {
+          $scope.studentInfo = student;
 
-				reformatted[camelCased] = object[field];
-			}
-		}
+          $scope.studentInfo.dob = moment($scope.studentInfo.dob, 'YYYY-MM-DD').toDate();
 
-		return reformatted;
-	}
+          if ($scope.studentInfo.belt) {
+            $scope.beltStyle = getBeltStyle($scope.studentInfo.belt);
 
-	function StudentDetailController($scope, $stateParams, StudentsService, apiHost, FileUploader, SharedDataSvc, $cookies) {
-		$scope.apiHost = apiHost;
+            $scope.earnedStripes = $scope.studentInfo.stripes.filter(function(personStripe) {
+              return personStripe['current_stripe'];
+            }).map(function(personStripe) {
+              return personStripe.stripe;
+            });
 
-		/* function to update this student object */
-		var updateStudent = function(){
-			StudentsService.getStudent($stateParams.studentId).then(
-	      function(response) {
-						$scope.studentInfo = reformatObject(response.data);
-
-			  SharedDataSvc.setStudentDob($scope.studentInfo.dob);
-	          $scope.studentInfo.dob = moment($scope.studentInfo.dob, 'YYYY-MM-DD').toDate();
-
-	          $scope.primaryEmergencyContact   = reformatObject($scope.studentInfo.emergencyContact1);
-	          $scope.secondaryEmergencyContact = reformatObject($scope.studentInfo.emergencyContact2);
-
-	          if ($scope.studentInfo.belt) {
-	              $scope.beltStyle = getBeltStyle($scope.studentInfo.belt);
-
-	              $scope.earnedStripes = $scope.studentInfo.stripes.filter(function(personStripe) {
-	                  return personStripe['current_stripe'];
-	              }).map(function(personStripe) {
-	                  return personStripe.stripe;
-	              });
-
-	              $scope.studentLoaded = true;
-	          } else {
-	              $scope.studentLoaded = true;
-	          }
-	      },
-        function(error) {
             $scope.studentLoaded = true;
+          } else {
+            $scope.studentLoaded = true;
+          }
+        },
+        function(error) {
+          $scope.studentLoaded = true;
 
-            if (error.status === 404) {
-                $scope.studentDoesNotExist = true;
-            } else {
-                $scope.studentRequestFailed = true;
-            }
+          if (error.status === 404) {
+            $scope.studentDoesNotExist = true;
+          } else {
+            $scope.studentRequestFailed = true;
+          }
         });
-		};
+    };
 
-		/* initialize the file uploader */
-		$scope.uploader = new FileUploader({
-			url: apiHost + '/api/person/' + $stateParams.studentId + '/picture',
-			autoUpload: true,
-			onCompleteAll: updateStudent,
-			headers: {
-				Authorization: 'Token ' + $cookies.getObject('Authorization').token
-			}
-		});
+    /* initialize the file uploader */
+    $scope.uploader = new FileUploader({
+      url: apiHost + '/api/person/' + $stateParams.studentId + '/picture',
+      autoUpload: true,
+      onCompleteAll: updateStudent,
+      headers: {
+        Authorization: 'Token ' + $cookies.getObject('Authorization').token
+      }
+    });
 
-		function getBeltStyle(belt) {
-			var primaryStyle = belt['primary_color'].toLowerCase() === 'ffffff' ?
-				'black 8px double' :
-				'#' + belt['primary_color'] + ' 8px solid';
+    function getBeltStyle(belt) {
+      var primaryStyle = belt['primary_color'].toLowerCase() === 'ffffff' ?
+        'black 8px double' :
+        '#' + belt['primary_color'] + ' 8px solid';
 
-				var secondaryStyle = belt['secondary_color'].toLowerCase() === 'ffffff' ?
-					'black 8px double' :
-					'#' + belt['secondary_color'] + ' 8px solid';
+      var secondaryStyle = belt['secondary_color'].toLowerCase() === 'ffffff' ?
+        'black 8px double' :
+        '#' + belt['secondary_color'] + ' 8px solid';
 
-			return {
-				'border-right': secondaryStyle,
-				'border-left': primaryStyle,
-				'border-top': primaryStyle,
-				'border-bottom': secondaryStyle
-			};
-		}
+      return {
+        'border-right': secondaryStyle,
+        'border-left': primaryStyle,
+        'border-top': primaryStyle,
+        'border-bottom': secondaryStyle
+      };
+    }
 
-		$scope.currentAge = function() {
-			if ($scope.studentInfo === undefined || $scope.studentInfo.dob === undefined) {
-				return null;
-			}
+    $scope.currentAge = function() {
+      if ($scope.studentInfo === undefined || $scope.studentInfo.dob === undefined) {
+        return null;
+      }
 
-			var today = moment();
-			var birthday = moment($scope.studentInfo.dob);
+      var today = moment();
+      var birthday = moment($scope.studentInfo.dob);
 
-			return today.diff(birthday, 'years');
-		};
+      return today.diff(birthday, 'years');
+    };
 
-		$scope.formattedBirthday = function() {
-			if ($scope.studentInfo === undefined || $scope.studentInfo.dob === undefined) {
-				return null;
-			}
+    $scope.formattedBirthday = function() {
+      if ($scope.studentInfo === undefined || $scope.studentInfo.dob === undefined) {
+        return null;
+      }
 
-			return moment($scope.studentInfo.dob).format('MM/DD/YYYY');
-		};
+      return moment($scope.studentInfo.dob).format('MM/DD/YYYY');
+    };
 
-		$scope.getFormattedEmailList = function() {
-			if ($scope.studentInfo === undefined || $scope.studentInfo.emails === undefined) {
-				return null;
-			}
+    $scope.getFormattedEmailList = function() {
+      if ($scope.studentInfo === undefined || $scope.studentInfo.emails === undefined) {
+        return null;
+      }
 
-			return $scope.studentInfo.emails.map(function(email) { return email.email; }).join(', ');
-		};
+      return $scope.studentInfo.emails.map(function(email) {
+        return email.email; }).join(', ');
+    };
 
-		$scope.studentInfo = {};
-		$scope.earnedStripes = [];
-		$scope.studentBeltClass = '';
+    $scope.studentInfo = {};
+    $scope.earnedStripes = [];
+    $scope.studentBeltClass = '';
 
-		$scope.primaryEmergencyContact = {};
-		$scope.secondaryEmergencyContact = {};
+    $scope.primaryEmergencyContact = {};
+    $scope.secondaryEmergencyContact = {};
 
-		$scope.studentLoaded = false;
-		$scope.studentRequestFailed = false;
-		$scope.studentDoesNotExist = false;
+    $scope.studentLoaded = false;
+    $scope.studentRequestFailed = false;
+    $scope.studentDoesNotExist = false;
 
-		updateStudent();
+    updateStudent();
+  }
 
-    SharedDataSvc.setStudentId($stateParams.studentId);
-	}
-
-	angular.module('ttkdApp.studentDetailCtrl', [
-		'ttkdApp.studentsService',
-		'ttkdApp.attendanceService',
-		'ttkdApp.telLinkDir',
-		'ttkdApp.constants',
-		'angularFileUpload',
-		'ngCookies'
-	]).controller('StudentDetailCtrl', [
-			'$scope',
-			'$stateParams',
-			'StudentsSvc',
-			'apiHost',
-			'FileUploader',
-			'SharedDataSvc',
-			'$cookies',
-			StudentDetailController
-		]);
+  angular.module('ttkdApp.studentDetailCtrl', [
+    'ttkdApp.studentsService',
+    'ttkdApp.attendanceService',
+    'ttkdApp.telLinkDir',
+    'ttkdApp.constants',
+    'angularFileUpload',
+    'ngCookies'
+  ]).controller('StudentDetailCtrl', [
+    '$scope',
+    '$stateParams',
+    'StudentsSvc',
+    'apiHost',
+    'FileUploader',
+    'SharedDataSvc',
+    '$cookies',
+    StudentDetailController
+  ]);
 })();
--- ttkd_ui/app/components/students/detail/view/student_waiver/waiver.controller.js
+++ ttkd_ui/app/components/students/detail/view/student_waiver/waiver.controller.js
@@ -1,5 +1,5 @@
 (function () {
-    function StudentWaiverController($scope, $http, $stateParams, apiHost, FileUploader, SharedDataService, $cookies, $document, $uibModal, $filter) {
+    function StudentWaiverController($scope, $http, apiHost, FileUploader, SharedDataService, $cookies, $document, $uibModal, $filter) {
         var modalInstance;
         $scope.waivers = [];
         $scope.hasWaivers = false;
@@ -11,6 +11,8 @@
             open: false,
             value: null
         };
+
+        var studentPromise = SharedDataService.getActiveStudent();
 
         $scope.uploadWaiver = function (waiverId) {
             document.getElementById('waiver-upload' + waiverId).click();
@@ -29,14 +31,10 @@
                 templateUrl: 'components/students/detail/view/student_waiver/waiver.modal.html',
                 scope: $scope
             });
-
-            modalInstance.result.then(function (selectedItem) {
-            }, function () {
-            });
         };
 
         $scope.cancel = function () {
-            modalInstance.dismiss('cancel');
+            modalInstance.close();
         };
 
         //opens the popup date picker window
@@ -58,49 +56,50 @@
 
             $http.post(apiHost + '/api/waivers/', payload).then(
                 function (response) {
-                    modalInstance.dismiss('create');
+                    modalInstance.close();
                     $scope.getWaivers();
                 }
             );
         };
 
         $scope.getWaivers = function () {
-            var waiversEndpoint = apiHost + '/api/waivers/?person=' + $stateParams.studentId;
-            $scope.waivers = [];
+            studentPromise.then(function(student) {
+                var waiversEndpoint = apiHost + '/api/waivers/?person=' + student.id;
+                $scope.waivers = [];
 
-            $http.get(waiversEndpoint).then(
-                function (response) {
-                    $scope.waivers = response.data;
-                    $scope.hasWaivers = ($scope.waivers.length > 0);
+                $http.get(waiversEndpoint).then(
+                    function (response) {
+                        $scope.waivers = response.data;
+                        $scope.hasWaivers = ($scope.waivers.length > 0);
 
-                    for (var i = 0; i < $scope.waivers.length; i++) {
+                        for (var i = 0; i < $scope.waivers.length; i++) {
 
-                        var waiverUrl = $scope.waivers[i]['waiver_url'];
+                            var waiverUrl = $scope.waivers[i]['waiver_url'];
 
-                        // If the waiver_url is not null lets fix the url by adding the api host
-                        if (waiverUrl !== undefined && waiverUrl !== null) {
-                            $scope.waivers[i]['waiver_url'] = apiHost + '/' + $scope.waivers[i]['waiver_url'];
+                            // If the waiver_url is not null lets fix the url by adding the api host
+                            if (waiverUrl !== undefined && waiverUrl !== null) {
+                                $scope.waivers[i]['waiver_url'] = apiHost + '/' + $scope.waivers[i]['waiver_url'];
+                            }
+
+                            $scope.waivers[i].formattedDate =
+                                moment($scope.waivers[i]['signature_date']).format('MM/DD/YYYY');
+
+                            $scope.waivers[i].waiverUploader = new FileUploader({
+                                url: apiHost + '/api/waiver/' + $scope.waivers[i].id + '/image',
+                                autoUpload: true,
+                                headers: {
+                                    Authorization: 'Token ' + $cookies.getObject('Authorization').token
+                                }
+                            });
+
+                            $scope.waivers[i].waiverUploader.onSuccessItem = function () {
+                                $scope.getWaivers();
+                            };
+                            $scope.waivers[i].age = moment($scope.waivers[i]['signature_date']).diff(moment(student.dob), 'years');
                         }
 
-                        $scope.waivers[i].formattedDate =
-                            moment($scope.waivers[i]['signature_date']).format('MM/DD/YYYY');
-
-                        $scope.waivers[i].waiverUploader = new FileUploader({
-                            url: apiHost + '/api/waiver/' + $scope.waivers[i].id + '/image',
-                            autoUpload: true,
-                            headers: {
-                                Authorization: 'Token ' + $cookies.getObject('Authorization').token
-                            }
-                        });
-
-                        $scope.waivers[i].waiverUploader.onSuccessItem = function () {
-                            $scope.getWaivers();
-                        };
-
-                        $scope.waivers[i].age = moment($scope.waivers[i]['signature_date']).diff(moment(SharedDataService.getStudentDob()), 'years');
-                    }
-
                 });
+            });
         };
 
         $scope.getWaivers();
@@ -110,7 +109,6 @@
         .controller('StudentWaiverCtrl', [
             '$scope',
             '$http',
-            '$stateParams',
             'apiHost',
             'FileUploader',
             'SharedDataSvc',
