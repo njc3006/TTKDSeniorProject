261e066b08313aaf9162a780bfe557246616e248
Andrew Vogler <axv3658@rit.edu>
2017-03-06 16:40:15 -0500

    Updated controller to post new notes, stripes, belts, and programs correctly

--- ttkd_ui/app/components/students/detail/edit/student.edit.controller.js
+++ ttkd_ui/app/components/students/detail/edit/student.edit.controller.js
@@ -170,7 +170,7 @@
 					state: $scope.studentInfo.state.value
 				});
 
-				StudentsService.updateStudentInfo($stateParams.studentId, payload).then(
+				StudentsService.updateStudentInfo($stateParams.studentId, payload, $scope.userlevel).then(
 					function success(response) {
 
 						$scope.oldStudent = angular.copy($scope.studentInfo);
