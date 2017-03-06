458f612decfeb1b508df5aef5ff73ec99995b1ab
Andrew Vogler <axv3658@rit.edu>
2017-03-06 13:34:16 -0500

    Condensed instructor student edit into existing student edit files

--- ttkd_ui/app/routes.js
+++ ttkd_ui/app/routes.js
@@ -106,12 +106,6 @@
 				controller: 'EditStudentCtrl'
 			})
 
-      .state('editStudentDetailsInstruct', {
-        url: '/students/{studentId:int}/instructor/edit',
-        templateUrl: 'components/students/detail/edit/student.instructor.edit.html',
-        controller: 'EditStudentCtrl'
-      })
-
       .state('beltsStripes', {
         url: '/belts-stripes',
         templateUrl: 'components/belts-stripes/belts-stripes.html',
