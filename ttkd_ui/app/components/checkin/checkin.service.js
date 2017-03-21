494d0db78eb6d59fccb500afc2395c6cc32695db
Nick Coriale <njc3006@rit.edu>
2017-03-20 21:20:42 -0400

    More tabs to spaces fixes

--- ttkd_ui/app/components/checkin/checkin.service.js
+++ ttkd_ui/app/components/checkin/checkin.service.js
@@ -1,53 +1,53 @@
-(function() {
-	function CheckinService($http, apiHost) {
+(function () {
+    function CheckinService($http, apiHost) {
 
-		var checkinMode = 'Checkin';
+        var checkinMode = 'Checkin';
 
-		return {
-			getCheckinMode: function () {
-				return checkinMode;
+        return {
+            getCheckinMode: function () {
+                return checkinMode;
             },
 
-			setCheckinMode: function (mode) {
-				checkinMode = mode;
+            setCheckinMode: function (mode) {
+                checkinMode = mode;
             },
 
-			getStudentsFromProgram: function(programId) {
-				return $http.get(apiHost + '/api/registrations-minimal/?program=' + programId + '&person__active=2');
-			},
+            getStudentsFromProgram: function (programId) {
+                return $http.get(apiHost + '/api/registrations-minimal/?program=' + programId + '&person__active=2');
+            },
 
-			getCheckinsForProgram: function(programId, checkinDate) {
-				return $http.get(apiHost + '/api/check-ins/?program=' + programId +
-					'&date=' + checkinDate);
-			},
+            getCheckinsForProgram: function (programId, checkinDate) {
+                return $http.get(apiHost + '/api/check-ins/?program=' + programId +
+                    '&date=' + checkinDate);
+            },
 
-			createCheckin: function(data) {
-				return $http.post(apiHost + '/api/check-ins/', data);
-			},
+            createCheckin: function (data) {
+                return $http.post(apiHost + '/api/check-ins/', data);
+            },
 
-			deleteCheckin: function(checkinId) {
-				return $http.delete(apiHost + '/api/check-ins/' + checkinId + '/');
-			},
+            deleteCheckin: function (checkinId) {
+                return $http.delete(apiHost + '/api/check-ins/' + checkinId + '/');
+            },
 
-			getInstructorsForProgram: function(programId) {
-				return $http.get(apiHost + '/api/instructors-minimal/?program=' + programId);
-			},
+            getInstructorsForProgram: function (programId) {
+                return $http.get(apiHost + '/api/instructors-minimal/?program=' + programId);
+            },
 
-			getInstructorCheckinsForProgram: function(programId, checkinDate) {
-				return $http.get(apiHost + '/api/instructor-check-ins/?program=' + programId +
-					'&date=' + checkinDate);
-			},
+            getInstructorCheckinsForProgram: function (programId, checkinDate) {
+                return $http.get(apiHost + '/api/instructor-check-ins/?program=' + programId +
+                    '&date=' + checkinDate);
+            },
 
-			createInstructorCheckin: function(data) {
-				return $http.post(apiHost + '/api/instructor-check-ins/', data);
-			},
+            createInstructorCheckin: function (data) {
+                return $http.post(apiHost + '/api/instructor-check-ins/', data);
+            },
 
-			deleteInstructorCheckin: function(checkinId) {
-				return $http.delete(apiHost + '/api/instructor-check-ins/' + checkinId + '/');
-			}
-		};
-	}
+            deleteInstructorCheckin: function (checkinId) {
+                return $http.delete(apiHost + '/api/instructor-check-ins/' + checkinId + '/');
+            }
+        };
+    }
 
-	angular.module('ttkdApp')
-		.factory('CheckinService', ['$http', 'apiHost', CheckinService]);
+    angular.module('ttkdApp')
+        .factory('CheckinService', ['$http', 'apiHost', CheckinService]);
 })();
