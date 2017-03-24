a9b153d01a2706182f1874ea5a0c1c249fcad600
Curtis Cali <ccali1993@gmail.com>
2017-03-23 23:28:27 -0400

    change format of display for achievements

--- ttkd_ui/app/components/students/detail/view/belt_stripe_history/belts-stripes-history.controller.js
+++ ttkd_ui/app/components/students/detail/view/belt_stripe_history/belts-stripes-history.controller.js
@@ -7,35 +7,74 @@
 			return dateB.getTime() - dateA.getTime();
 		}
 
-		$scope.formatDate = function(date) {
-			return moment(date, 'YYYY-MM-DD').format('MM/DD/YYYY');
+		function formatDate(date) {
+			return moment(date).format('MM/DD/YYYY');
 		}
 
+		$scope.makeHistoryText = function(record) {
+			var text = record.name;
+
+			if (record.type === 'stripe') {
+				text += ' Stripe Achieved ';
+			} else {
+				text += ' Belt Achieved ';
+			}
+
+			text += formatDate(record.date);
+
+			return text;
+		};
+
 		SharedDataService.getActiveStudent().then(function(student) {
-			$scope.loadingBeltHistory = true;
-			$scope.loadingStripeHistory = true;
+			$scope.loadingInProgress = true;
+			$scope.loadingFailed = false;
 
 			StudentsService.getBeltHistoryForStudent(student.id).then(
 				function success(response) {
-					$scope.beltHistory = response.data.sort(compareDatesInverse);
-					$scope.loadingBeltHistory = false;
-				},
-				function failure(error) {
-					console.error(error);
-					$scope.loadingBeltHistoryFailed = true
-					$scope.loadingBeltHistory = false;
-				}
-			);
+					$scope.history = response.data.map(function(personBelt) {
+						return {
+							type: 'belt',
+							date: moment(personBelt['date_achieved'], 'YYYY-MM-DD').toDate(),
+							name: personBelt.belt.name
+						};
+					});
 
-			StudentsService.getStripeHistoryForStudent(student.id).then(
-				function success(response) {
-					$scope.stripeHistory = response.data.sort(compareDatesInverse);
-					$scope.loadingStripeHistory = false;
+					StudentsService.getStripeHistoryForStudent(student.id).then(
+						function success(response) {
+							$scope.history = $scope.history.concat(response.data.map(function(personStripe) {
+								return {
+									type: 'stripe',
+									date: moment(personStripe['date_achieved'], 'YYYY-MM-DD').toDate(),
+									name: personStripe.stripe.name
+								};
+							}));
+
+							$scope.history = $scope.history.sort(function(a, b) {
+								var momentA = moment(a.date),
+									momentB = moment(b.date);
+
+								if (momentB.isSame(momentA, 'day') && b.type === 'belt') {
+									return 1;
+								} else if (momentB.isSame(momentA, 'day') && b.type === 'stripe') {
+									return -1;
+								}
+
+								return b.date.getTime() - a.date.getTime();
+							});
+
+							$scope.loadingInProgress = false;
+						},
+						function failure(error) {
+							console.error(error);
+							$scope.loadingFailed = true;
+							$scope.loadingInProgress = false;
+						}
+					);
 				},
 				function failure(error) {
 					console.error(error);
-					$scope.loadingStripeHistoryFailed = true
-					$scope.loadingStripeHistory = false;
+					$scope.loadingInProgress = false;
+					$scope.loadingFailed = true;
 				}
 			);
 		});
