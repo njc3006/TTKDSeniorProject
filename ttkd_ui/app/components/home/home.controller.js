0152a7bc229dbde19f89f17b71cb09d0373d0260
Andrew Vogler <axv3658@rit.edu>
2017-04-09 10:37:01 -0400

    Moved add program button to the programs page

--- ttkd_ui/app/components/home/home.controller.js
+++ ttkd_ui/app/components/home/home.controller.js
@@ -8,10 +8,6 @@
 
     	var modalInstance;
      	$scope.programs = [];
-        //programs entered into the add program modal field will be temporarily stored here
-        $scope.newProgram = '';
-        //messages to be relayed to user when adding programs
-        $scope.addProgramMessage = {};
 
         var getActivePrograms = function() {
             ProgramsSvc.getActivePrograms().then(function onSuccess(response) {
@@ -23,36 +19,6 @@
 					$state.go(page, data);
 				};
 
-        $scope.openAddProgram = function() {
-            $scope.addProgramMessage = {};
-            var modalElement = angular.element($document[0].querySelector('#modal-area'));
-            modalInstance = $uibModal.open({
-                animation: true,
-                ariaLabelledBy: 'modal-title',
-                ariaDescribedBy: 'modal-body',
-                templateUrl: 'components/home/add-program.modal.html',
-                scope: $scope
-            });
-        };
-
-        /* add the typed in program */
-        $scope.addProgram = function(program) {
-            if(program !== '') {
-                var postData = {name: program};
-                ProgramsSvc.postNewProgram(postData).then(
-                    function onSuccess(response) {
-                        $scope.addProgramMessage = {success: 'Successfuly added ' + program + '.'};
-                        $scope.newProgram = '';
-                        getActivePrograms();
-                    }, function onFailure(response) {
-                        $scope.addProgramMessage = {
-                            error: 'Failed to add ' + program +
-                            '. Please make sure this program does not already exist.'
-                        };
-                        $scope.newProgram = '';
-                    });
-            }
-        };
 
     	$scope.openChangeProgram = function() {
             getActivePrograms();
