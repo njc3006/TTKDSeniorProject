48a307218f2a8bb9ac300c14c21fbba23703853e
Curtis Cali <ccali1993@gmail.com>
2017-04-13 10:22:58 -0400

    fix indentation

--- ttkd_ui/protractor.config.js
+++ ttkd_ui/protractor.config.js
@@ -1,8 +1,8 @@
 // Configuration for Protractor.js Test Framework
 exports.config = {
   seleniumServerJar: './node_modules/gulp-protractor/node_modules/protractor/node_modules/webdriver-manager/selenium/selenium-server-standalone-2.53.1.jar',
-  specs: ['app/components/registration/**/*.test.js'],
-	rootElement: '#appContainer',
+  specs: ['app/components/**/*.test.js'],
+  rootElement: '#appContainer',
   params: {
     appUrl: 'http://localhost:3000/#!/'
   },
