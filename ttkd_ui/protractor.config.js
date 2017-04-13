2d1c3652787ace92d551cb7df6fb48ef881e86a2
Curtis Cali <ccali1993@gmail.com>
2017-04-13 13:30:09 -0400

    change config file back

--- ttkd_ui/protractor.config.js
+++ ttkd_ui/protractor.config.js
@@ -1,7 +1,7 @@
 // Configuration for Protractor.js Test Framework
 exports.config = {
   seleniumServerJar: './node_modules/gulp-protractor/node_modules/protractor/node_modules/webdriver-manager/selenium/selenium-server-standalone-2.53.1.jar',
-  specs: ['app/components/students/detail/**/*.test.js'],
+  specs: ['app/components/**/*.test.js'],
   params: {
     appUrl: 'http://localhost:3000/#!/'
   },
